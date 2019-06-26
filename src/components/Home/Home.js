3139feeb7e6ae508adcc82aa1a4d13e53deb0b77
Bilal Koçak <kocakbilal04@gmail.com>
2019-06-26 23:17:43 +0300

    github button setted

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import './Home.css'
 
 class Home extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: ''
+        }
+    }
+
+
+    buttonClickHandler = (e) => {
+        let id = e.target.id;
+        this.props.mode(id);
+    };
+
+    goToGithub = () => {
+        window.location.href = 'https://github.com/bilalkocak/decimalgame';
+    };
 
     componentDidMount() {
 
@@ -13,20 +30,20 @@ class Home extends React.Component {
         return (
             <div className="home">
 
-                <div className="header">
-
+                <div className="homeHeader">
+                    Decimal Game
                 </div>
                 <div className="buttonContainer">
-                    <div className="button" id={'easy'}>
+                    <div className="button" id={'easy'} onClick={this.buttonClickHandler}>
                         EASY (4x4)
                     </div>
-                    <div className="button" id={'medium'}>
+                    <div className="button" id={'medium'} onClick={this.buttonClickHandler}>
                         MEDIUM (6x6)
                     </div>
-                    <div className="button" id={'hard'}>
+                    <div className="button" id={'hard'} onClick={this.buttonClickHandler}>
                         HARD (8x8)
                     </div>
-                    <div className="button" id={'github'}>
+                    <div className="button" id={'github'} onClick={this.goToGithub}>
                         GITHUB
                     </div>
                 </div>
